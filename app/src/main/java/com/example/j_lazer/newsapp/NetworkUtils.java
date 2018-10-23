ee40027d15719f969663c42ade19c07414dbbab5
JAUSH <j05h14@yahoo.com>
2018-10-23 11:35:06 -0700

    first commit

---
+++ app/src/main/java/com/example/j_lazer/newsapp/NetworkUtils.java
@@ -0,0 +1,6 @@
+package com.example.j_lazer.newsapp;
+
+public class NetworkUtils {
+    final static String BASE_URL = "https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=9a4e1c9d357a47b9afe4bbe54e2dfe2f";
+    final static String PARAM_QUEREY = "q";
+}
